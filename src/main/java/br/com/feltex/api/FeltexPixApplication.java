2d7c0f5de17f24a836580664288ce48d474bbb07
Andre Felix <andrefelix.br@gmail.com>
2024-09-11 19:43:02 +0100

    initial version

---
+++ src/main/java/br/com/feltex/api/FeltexPixApplication.java
@@ -0,0 +1,13 @@
+package br.com.feltex.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeltexPixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeltexPixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/feltex/api/FeltexPixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.feltex.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeltexPixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
